a516ba38033c15cd2970ffa8067d49f17b14c99c
arqamshakeel <arqam.android@gmail.com>
2020-12-27 04:28:18 +0500

    Update home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col, Empty } from "antd";
-import axios from 'axios';
+import axios from "axios";
 import Card from "../../components/Card";
 
 class Home extends Component {
@@ -28,18 +28,27 @@ class Home extends Component {
     };
     return (
       <>
-       <h2 style={{marginBottom: "50px"}}>Pick your favourite meal and click to place the order:<br></br></h2>
+        <h2 style={{ marginBottom: "50px" }}>
+          Pick your favourite meal and click to place the order:<br></br>
+        </h2>
 
         {products && products.length ? (
           <div className="site-card-wrapper">
             <Row gutter={[16, 24]}>
-              {products.map((product) => {
-                return (
-                  <Col span={6} key={product.id}>
-                    <Card handleCardClick={handleCardClick} product={product} />
-                  </Col>
-                );
-              })}
+              {products ? (
+                products.map((product) => {
+                  return (
+                    <Col span={6} key={product.id}>
+                      <Card
+                        handleCardClick={handleCardClick}
+                        product={product}
+                      />
+                    </Col>
+                  );
+                })
+              ) : (
+                <></>
+              )}
             </Row>
           </div>
         ) : (
